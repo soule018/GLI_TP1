31160089601d4f4acc92a6856a01596b60a4b04d
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2020-11-12 15:17:36 +0100

    suite Q4-7

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -8,11 +8,18 @@ import { Pokemon } from '../pokemon';
 })
 export class MyComponentComponent implements OnInit {
   id: string = '';
+  selectedPockeId : string;
 
 
     pok : Pokemon [] = [];
 
-  constructor() { }
+  constructor() { 
+    this.pok.push (new Pokemon ('1','Reptincel'));
+    this.pok.push (new Pokemon ('2','Bulbizarre'));
+    this.pok.push (new Pokemon ('3','Herbizarre'));
+    this.pok.push (new Pokemon ('4','Florizarre'));
+    this.pok.push (new Pokemon ('5','Salamèche'));
+  }
 
   ngOnInit(): void {
   }
