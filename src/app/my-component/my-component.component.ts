4d85781fd40640e2084787ad838f01a84c591836
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2020-11-12 19:43:36 +0100

    Question 10-12

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeAPIServiceService } from '../poke-apiservice.service';
 import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
-  styleUrls: ['./my-component.component.css']
+  styleUrls: ['./my-component.component.css'],
+  providers: [PokeAPIServiceService]
 })
 export class MyComponentComponent implements OnInit {
   id: string = '';
@@ -12,15 +14,24 @@ export class MyComponentComponent implements OnInit {
   searchPokeName = '';
   pok : Pokemon [] = [];
 
-  constructor() { 
-    this.pok.push (new Pokemon ('1','Reptincel'));
+  constructor( private pokeService: PokeAPIServiceService) { 
+    /*this.pok.push (new Pokemon ('1','Reptincel'));
     this.pok.push (new Pokemon ('2','Bulbizarre'));
     this.pok.push (new Pokemon ('3','Herbizarre'));
     this.pok.push (new Pokemon ('4','Florizarre'));
-    this.pok.push (new Pokemon ('5','Salamèche'));
+    this.pok.push (new Pokemon ('5','Salamèche'));*/
   }
 
   ngOnInit(): void {
+    this.pokeService.getPokemons().subscribe((data) => {
+
+      console.log(data.results);
+
+      data.results.forEach ((e, index) => {
+        this.pok.push (new Pokemon (index,e.name,e.url));
+
+      });
+    }) ;
   }
   go(){
     console.log (this.selectedPockeId);
--- src/app/pokemon.ts
+++ src/app/pokemon.ts
@@ -1,4 +1,4 @@
 export class Pokemon {
 
-    constructor( public id:string, public  name:string){}
+    constructor( public id:string, public  name:string, public url: string){}
 }
